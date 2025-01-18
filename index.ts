701afa71cb7c07d85329b5e25e65025688ed4202
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-18 14:07:07 -0400

    feat: Use client router

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
-
-import authRouter from "@routers/auth";
-import userRouter from "@routers/users";
+import { AuthRouter, ClientRouter, UserRouter } from "@routers";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use("/auth", authRouter);
-app.use("/users", userRouter);
+app.use("/auth", AuthRouter);
+app.use("/user", UserRouter);
+app.use("/client", ClientRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
