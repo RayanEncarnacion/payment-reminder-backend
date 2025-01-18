bde4f8315453f38899098076237d44db24a10b6c
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-18 14:06:44 -0400

    feat: Services and controllers barrel file

---
+++ src/controllers/index.ts
@@ -0,0 +1,3 @@
+export { default as AuthController } from "@controllers/auth";
+export { default as UserController } from "@controllers/user";
+export { default as ClientController } from "@controllers/client";
---
+++ src/services/index.ts
@@ -0,0 +1,3 @@
+export { default as AuthService } from "./auth";
+export { default as UserService } from "./user";
+export { default as ClientService } from "./client";
