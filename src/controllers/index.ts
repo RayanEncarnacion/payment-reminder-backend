2cbbce011d8ee0101e6e3ca60f2e8c411dc3c2e9
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-19 11:07:20 -0400

    feat: Update services, controllers and routers barrel files to import project files

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as AuthController } from "@controllers/auth";
 export { default as UserController } from "@controllers/user";
 export { default as ClientController } from "@controllers/client";
+export { default as ProjectController } from "@controllers/project";
