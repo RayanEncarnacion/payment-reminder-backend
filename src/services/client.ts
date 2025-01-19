3652d18345b51550184d65a416177d9ee92dd9f5
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-19 16:34:44 -0400

    feat: Extend base service and add getById method

--- src/services/client.ts
+++ src/services/client.ts
@@ -3,21 +3,20 @@ import { clientsTable, projectsTable } from "@db/schemas";
 import { eq } from "drizzle-orm";
 import { db } from "@db";
 import { updateClientPayload } from "@validation/schemas";
+import { BaseService } from "./base";
 
-class ClientService {
+class ClientService extends BaseService {
+  async existsById(id: number) {
+    return await super.existsById(id, clientsTable);
+  }
+
   async create(client: typeof clientsTable.$inferInsert) {
     const [{ id }] = await db
       .insert(clientsTable)
       .values(client)
       .$returningId();
 
-    return (
-      await db
-        .select()
-        .from(clientsTable)
-        .where(eq(clientsTable.id, id))
-        .limit(1)
-    )[0];
+    return await this.getById(id);
   }
 
   async getAll() {
@@ -53,6 +52,16 @@ class ClientService {
       .select()
       .from(projectsTable)
       .where(eq(projectsTable.clientId, id));
+  }
+
+  async getById(id: number) {
+    return (
+      await db
+        .select()
+        .from(clientsTable)
+        .where(eq(clientsTable.id, id))
+        .limit(1)
+    )[0];
   }
 }
 
