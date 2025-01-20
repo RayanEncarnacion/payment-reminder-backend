0d7ea9b7dd48fef401370ae867380b4321734a44
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-19 20:39:29 -0400

    feat: Validate properties on create and update

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -1,14 +1,16 @@
 import { Response, Request } from "express";
 import { createClientPayload } from "@validation/schemas";
 import { ClientService } from "@services";
+import { StatusCodes } from "http-status-codes";
 
 class ClientController {
   async getAll(req: Request, res: Response) {
     try {
-      const clients = await ClientService.getAll();
-      res.status(200).json({ success: true, data: clients });
+      res
+        .status(200)
+        .json({ success: true, data: await ClientService.getAll() });
     } catch (error: any) {
-      res.status(500).json({
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         success: false,
         error: error?.message || "Something went wrong. Try again later.",
       });
@@ -16,12 +18,26 @@ class ClientController {
   }
 
   async create(req: Request, res: Response) {
-    const { email, name } = req.body as createClientPayload;
+    const name = req.body.name.trim();
+    const email = req.body.email.trim();
+    const clientWithNameOrEmail = await ClientService.getByNameOrEmail(
+      name,
+      email
+    );
 
+    if (clientWithNameOrEmail) {
+      res.status(StatusCodes.CONFLICT).json({
+        success: false,
+        error: "The name or email already exist.",
+      });
+      return;
+    }
+
+    const token = (req as any).authToken;
     const client = await ClientService.create({
       email,
       name,
-      createdBy: 1, // TODO: Get id of user from token
+      createdBy: token.id,
     });
 
     res.status(201).json({
@@ -38,8 +54,24 @@ class ClientController {
   }
 
   async update(req: Request, res: Response) {
-    const { id } = req.params;
-    await ClientService.update(parseInt(id, 10), req.body);
+    const id = parseInt(req.params.id, 10);
+    const name = req.body.name.trim();
+    const email = req.body.email.trim();
+
+    const clientWithNameOrEmail = await ClientService.getByNameOrEmail(
+      name,
+      email
+    );
+
+    if (clientWithNameOrEmail && clientWithNameOrEmail.id != id) {
+      res.status(StatusCodes.CONFLICT).json({
+        success: false,
+        error: "The name or email already exist.",
+      });
+      return;
+    }
+
+    await ClientService.update(id, req.body);
 
     res.status(200).json({ success: true });
   }
