3b79c05ec71d188d1aaf12527c4d7b71a901e1df
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-19 21:32:24 -0400

    chore: Removed unused route

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -2,7 +2,4 @@ import express from "express";
 import _middleware from "@middleware";
 import { UserController } from "@controllers";
 
-export default express
-  .Router()
-  .get("/", UserController.getAll)
-  .get("/protected", _middleware.validateAuthToken, UserController.getAll);
+export default express.Router().get("/", UserController.getAll);
