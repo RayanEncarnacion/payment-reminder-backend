2dc87869b541a1655a73177898a418e3ce829ad4
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-19 21:35:26 -0400

    feat: Add delete user endpoint

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,13 +1,29 @@
 import { Response, Request } from "express";
 import { UserService } from "@services";
+import { StatusCodes } from "http-status-codes";
 
 class UserController {
   async getAll(req: Request, res: Response) {
     try {
-      const users = await UserService.getUsers();
-      res.status(200).json({ success: true, data: users });
+      res
+        .status(200)
+        .json({ success: true, data: await UserService.getUsers() });
     } catch (error: any) {
-      res.status(500).json({
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        error: error?.message || "Something went wrong. Try again later.",
+      });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      await UserService.delete(parseInt(id, 10));
+
+      res.status(204).send();
+    } catch (error: any) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         success: false,
         error: error?.message || "Something went wrong. Try again later.",
       });
--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,5 +1,14 @@
 import express from "express";
 import _middleware from "@middleware";
 import { UserController } from "@controllers";
+import { idParamSchema } from "@validation/schemas";
 
-export default express.Router().get("/", UserController.getAll);
+export default express
+  .Router()
+  .get("/", UserController.getAll)
+  .delete(
+    "/:id",
+    _middleware.validateAuthToken,
+    _middleware.validateParams(idParamSchema),
+    UserController.delete
+  );
