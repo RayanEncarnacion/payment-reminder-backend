40933680db5996156e43d6a223eaae3c79f59c49
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-21 16:36:26 -0400

    feat: Update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,40 @@
 import js from '@eslint/js'
 import prettierPlugin from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
+import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import importPlugin from 'eslint-plugin-import'
 
 export default [
   js.configs.recommended,
   {
+    files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
+      parser: typescriptParser,
       ecmaVersion: 'latest',
       sourceType: 'module',
+      globals: {
+        console: 'readonly',
+      },
     },
-  },
-  {
+
     plugins: {
+      '@typescript-eslint': typescriptPlugin,
       prettier: prettierPlugin,
+      import: importPlugin,
     },
     rules: {
+      ...typescriptPlugin.configs.recommended.rules,
       ...prettierConfig.rules,
       'prettier/prettier': 'error',
     },
-  },
-  {
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+    },
     rules: {
       'prettier/prettier': 'error',
-      'no-console': 'warn',
       'no-unused-vars': 'warn',
       eqeqeq: 'error',
       'no-var': 'error',
@@ -30,6 +42,24 @@ export default [
       'prefer-arrow-callback': 'error',
       'arrow-body-style': ['error', 'as-needed'],
       'import/no-unresolved': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal'],
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
       'import/named': 'error',
       'import/default': 'error',
       'import/namespace': 'error',
