16996cbc168a93348d030fd85cbbb5d1acf3806f
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-01-29 17:52:26 -0400

    feat: Implement removeFromList in redis service

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,10 +1,10 @@
-import { SetOptions } from 'redis'
 import { redisClient } from '@db/redis'
 
 export interface IRedisService {
   get(key: string): Promise<string | null>
-  set(key: string, value: any, options?: SetOptions): void
+  set(key: string, value: any, options?: { EX: number }): void
   addToList(key: string, value: any): void
+  removeFromList(listKey: string, identifier: any, identifierValue: any): void
 }
 
 class RedisService implements IRedisService {
@@ -12,7 +12,7 @@ class RedisService implements IRedisService {
     return await redisClient.get(key)
   }
 
-  async set(key: string, value: any, options: SetOptions = { EX: 300 }) {
+  async set(key: string, value: any, options: { EX: number } = { EX: 300 }) {
     redisClient.set(key, JSON.stringify(value), options)
   }
 
@@ -20,6 +20,20 @@ class RedisService implements IRedisService {
     const values = (await redisClient.get(key)) || '[]'
 
     redisClient.set(key, JSON.stringify([value, ...JSON.parse(values)]), {
+      EX: 300,
+    })
+  }
+
+  async removeFromList(listKey: string, identifier: any, identifierValue: any) {
+    const cachedList = JSON.parse((await redisClient.get(listKey)) || '[]')
+
+    if (!Array.isArray(cachedList))
+      throw new Error('Key provided does not contain a list')
+
+    const updatedValues = cachedList.filter(
+      (value: any) => value[identifier] !== identifierValue,
+    )
+    redisClient.set(listKey, JSON.stringify(updatedValues), {
       EX: 300,
     })
   }
