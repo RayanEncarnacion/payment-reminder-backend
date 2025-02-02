bb62461b49f497c01ed571eb45cc52efb2974624
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-01 22:07:56 -0400

    feat: Add remove method to IRedisService interface

--- src/services/types.ts
+++ src/services/types.ts
@@ -3,6 +3,7 @@
 export interface IRedisService {
   get(key: string): Promise<string | null>
   set(key: string, value: any, options?: { EX: number }): void
+  remove(key: string): void
   addToList(key: string, value: any): void
   removeListItem(listKey: string, identifier: any, identifierValue: any): void
   updateListItem(
