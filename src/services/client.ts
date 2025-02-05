13dac009216892cc0b1fb05d011d4e1c6cef9b60
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-05 19:12:56 -0400

    feat: Reference service internal tables (clients and projects) passed thru ctor

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,25 +1,46 @@
 import { eq, and, or } from 'drizzle-orm'
 import { db, clientsTable, projectsTable } from '@src/db'
-import { BaseService, RedisService } from '@src/services'
+import { BaseService, IRedisService, RedisService } from '@src/services'
 
-class ClientService extends BaseService<typeof clientsTable> {
+export class ClientService extends BaseService<typeof clientsTable> {
+  table: typeof clientsTable
+  _projectsTable: typeof projectsTable
+  redis: IRedisService
+  tableName: string
+
+  constructor(
+    table: typeof clientsTable,
+    tableName: string,
+    _projectsTable: typeof projectsTable,
+    redisService: IRedisService,
+  ) {
+    super(table, tableName, redisService)
+    this.table = table
+    this.tableName = tableName
+    this._projectsTable = _projectsTable
+    this.redis = redisService
+  }
+
   async getByEmail(email: string) {
     return (
-      await db.select().from(clientsTable).where(eq(clientsTable.email, email))
+      await db
+        .select()
+        .from(this.table)
+        .where(eq((this.table as any).email, email))
     )[0]
   }
 
   async delete(id: number) {
     await db.transaction(async (tx) => {
       await tx
-        .update(clientsTable)
+        .update(this.table)
         .set({ deleted: 1 })
-        .where(eq(clientsTable.id, id))
+        .where(eq(this.table.id, id))
 
       await tx
-        .update(projectsTable)
+        .update(this._projectsTable)
         .set({ deleted: 1 })
-        .where(eq(projectsTable.clientId, id))
+        .where(eq(this._projectsTable.clientId, id))
     })
 
     this.redis.removeListItem('clients', 'id', id)
@@ -28,16 +49,26 @@ class ClientService extends BaseService<typeof clientsTable> {
   async getWithProjects(id: number) {
     return await db
       .select()
-      .from(projectsTable)
-      .where(and(eq(projectsTable.clientId, id), eq(projectsTable.deleted, 0)))
+      .from(this._projectsTable)
+      .where(
+        and(
+          eq(this._projectsTable.clientId, id),
+          eq(this._projectsTable.deleted, 0),
+        ),
+      )
   }
 
   async getByNameOrEmail(name: string, email: string) {
     return await db
       .select()
-      .from(clientsTable)
-      .where(or(eq(clientsTable.email, email), eq(clientsTable.name, name)))
+      .from(this.table)
+      .where(or(eq(this.table.email, email), eq(this.table.name, name)))
   }
 }
 
-export default new ClientService(clientsTable, 'clients', RedisService)
+export default new ClientService(
+  clientsTable,
+  'clients',
+  projectsTable,
+  RedisService,
+)
