eefaecd0b8be1458e626714d575ac59f41c51246
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-06 11:52:49 -0400

    feat: Add client service delete tests

--- src/services/__tests__/client-service.test.ts
+++ src/services/__tests__/client-service.test.ts
@@ -5,6 +5,7 @@ jest.mock('../../db/db')
 const { db } = require('../../db/db')
 
 const mockClientsTable = getMockTable('clients')
+const mockProjectsTable = getMockTable('projects')
 
 describe('ClientService', () => {
   let service: ClientService
@@ -47,6 +48,49 @@ describe('ClientService', () => {
       const result = await service.getByEmail(row.email)
 
       expect(result).toEqual(undefined)
+    })
+  })
+
+  describe('delete', () => {
+    it('should delete client from db and redis and projects from db', async () => {
+      const rowId = 1
+      const mockTx = {
+        update: jest.fn().mockReturnThis(),
+        set: jest.fn().mockReturnThis(),
+        where: jest.fn().mockResolvedValue([{}]),
+      }
+      db.transaction.mockImplementation((callback: any) => callback(mockTx))
+
+      const result = await service.delete(rowId)
+
+      // ! Should update clients and projects tables, and update redis
+      expect(result).toEqual(true)
+      expect(mockTx.update).toHaveBeenCalledTimes(2)
+      expect(mockTx.update).toHaveBeenCalledWith(mockClientsTable)
+      expect(mockTx.update).toHaveBeenNthCalledWith(2, mockProjectsTable)
+      expect(mockRedisService.removeListItem).toHaveBeenCalledWith(
+        mockClientsTable.name,
+        'id',
+        rowId,
+      )
+    })
+
+    it('should not delete row with ID', async () => {
+      const rowId = 1
+      const mockTx = {
+        update: jest.fn().mockReturnThis(),
+        set: jest.fn().mockReturnThis(),
+        where: jest.fn().mockResolvedValue([]), // ! Returns empty rows updated
+      }
+      db.transaction.mockImplementation((callback: any) => callback(mockTx))
+
+      const result = await service.delete(rowId)
+
+      // ! Should not update rows in the clients table and avoid calling the redis service
+      expect(result).toEqual(undefined)
+      expect(mockTx.update).toHaveBeenCalledTimes(1)
+      expect(mockTx.update).toHaveBeenCalledWith(mockClientsTable)
+      expect(mockRedisService.removeListItem).not.toHaveBeenCalled()
     })
   })
 })
