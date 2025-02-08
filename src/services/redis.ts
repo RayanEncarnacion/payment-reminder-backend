31220d83cca5b0ba9a757514714a5554e2170892
Rayan Encarnacion <rayanbalbuena@gmail.com>
2025-02-08 16:00:20 -0400

    feat: Reference redisClient passed on instation

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,34 +1,41 @@
+import { RedisClientType } from 'redis'
 import { redisClient } from '@src/db'
 import { type IRedisService } from '@src/services'
 
-class RedisService implements IRedisService {
+export class RedisService<T extends RedisClientType<any, any, any>>
+  implements IRedisService
+{
+  private redis: T
+
+  constructor(redisClient: T) {
+    this.redis = redisClient
+  }
+
   async get(key: string) {
-    return await redisClient.get(key)
+    return await this.redis.get(key)
   }
 
   async remove(key: string) {
-    return await redisClient.del(key)
+    return await this.redis.del(key)
   }
 
   async set(key: string, value: string, options: { EX: number } = { EX: 300 }) {
-    redisClient.set(key, value, options)
+    return this.redis.set(key, value, options)
   }
 
   async addToList(key: string, value: any) {
-    const values = (await redisClient.get(key)) || '[]'
+    const values = (await this.redis.get(key)) || '[]'
 
-    redisClient.set(key, JSON.stringify([value, ...JSON.parse(values)]), {
-      EX: 300,
-    })
+    return this.redis.set(key, JSON.stringify([value, ...JSON.parse(values)]))
   }
 
   // eslint-disable-next-line no-unused-vars
-  private async updateList(listKey: string, filterFn: (list: any[]) => any[]) {
-    const cachedList = await redisClient.get(listKey)
+  async updateList(listKey: string, filterFn: (list: any[]) => any[]) {
+    const cachedList = await this.redis.get(listKey)
 
     if (!cachedList) return
     const updatedList = filterFn(JSON.parse(cachedList))
-    redisClient.set(listKey, JSON.stringify(updatedList), { EX: 300 })
+    return this.redis.set(listKey, JSON.stringify(updatedList))
   }
 
   async updateListItem(
@@ -42,15 +49,15 @@ class RedisService implements IRedisService {
         value[identifier] === identifierValue ? newValue : value,
       )
 
-    this.updateList(listKey, filterFn)
+    return this.updateList(listKey, filterFn)
   }
 
   async removeListItem(listKey: string, identifier: any, identifierValue: any) {
     const filterFn = (list: any[]) =>
       list.filter((value: any) => value[identifier] !== identifierValue)
 
-    this.updateList(listKey, filterFn)
+    return this.updateList(listKey, filterFn)
   }
 }
 
-export default new RedisService()
+export default new RedisService(redisClient)
